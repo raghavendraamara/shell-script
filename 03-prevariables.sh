7bf94d16ce5087d39f0fe57d522e3ddcb95ea640
raghavendraamara <raghavendraamara@gmail.com>
2025-01-06 16:40:19 +0530

    preassianed variables

--- 03-prevariables.sh
+++ 03-prevariables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1=$raju
-PERSON2=$ravi
+PERSON1=raju
+PERSON2=ravi
 echo "{PERSON1}:: hi {PERSON2}"
 echo "{PERSON2}:: good how are you {PERSON1}"
 echo "{PERSON1}:: fine what are you doing"
